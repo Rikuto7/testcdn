1c42a8bf0d6fe5edbb4fc180d85d76a9fbf94f92
Rikuto7 <rikuto@recustomer.co>
2025-06-18 16:59:52 +0900

    test

--- test.js
+++ test.js
@@ -1,4 +1,32 @@
 console.log("ファイル自体は読み込めてます")
-function logMessage(message) {
-    console.log(message);
+
+const displayType = {
+    BUTTON: "button",
+    TOGGLE: "toggle",
+    HIDDEN: "hidden",
+}
+
+const evaluateDisplayType = (
+    inventory,
+    tags,
+    category,
+    custom_attribute1,
+    custom_attributes_2
+) => {
+    if (inventory < 5) {
+        return displayType.HIDDEN;
+    }
+    if (tags.includes("お試しボタン")) {
+        return displayType.BUTTON;
+    }
+    if (category === "お試しトグル") {
+        return displayType.TOGGLE;
+    }
+    if (custom_attribute1 === "お試しボタン") {
+        return displayType.BUTTON;
+    }
+    if (custom_attributes_2 === "お試しトグル") {
+        return displayType.TOGGLE;
+    }
+    return displayType.HIDDEN;
 }
